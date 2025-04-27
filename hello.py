e9fadee102b509b8fe94947b30cac97229bb594d
Peter Lee <banklover@gmail.com>
2025-04-27 20:42:25 +0900

    changed code to show pop up window using TK

--- hello.py
+++ hello.py
@@ -1,7 +1,12 @@
+import tkinter as tk
+
 """
     Python code from my Mac
     Author: Peter Lee
     Date:   Jan. 3, 2023
 """
-
-print("Hello, Mac!")
+root = tk.Tk()
+root.title("Message")
+label = tk.Label(root, text="Hello, VS Code")
+label.pack(padx=20, pady=20)
+root.mainloop()
